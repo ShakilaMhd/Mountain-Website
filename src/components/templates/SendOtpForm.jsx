2f132c8b19327bd637cb9407876d7fc2737b3d1e
shakila mohammadi <57131482+ShakilaMhd@users.noreply.github.com>
2024-08-26 16:01:40 +0330

    give the mobile number of user and submit it

--- src/components/templates/SendOtpForm.jsx
+++ src/components/templates/SendOtpForm.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
-function SendOtpForm() {
+function SendOtpForm({ mobile, setMobile, setStep }) {
+    const submitHandler = (event) => {
+        event.preventDefault()
+        console.log(event);
+    }
   return (
-    <div>SendOtpForm</div>
-  )
+    <form onSubmit={submitHandler}>
+      <p>ورود به حساب کاربری</p>
+      <span>
+        برای استفاده از امکانات سایت // لطقا شماره موبایل خود را وارد کنید
+        کدتایید شده به این شماره پیامک خواهد شد
+      </span>
+      <label htmlFor="input">ماره موبایل خود را وارد کنید</label>
+          <input type="text" id="input" placeholder="شماره موبایل" value={mobile} onChange={e => setMobile(e.target.value)} />
+          <button type="submit">ارسال کد تایید</button>
+    </form>
+  );
 }
 
-export default SendOtpForm
+export default SendOtpForm;
--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -3,13 +3,15 @@ import SendOtpForm from "../components/templates/SendOtpForm";
 import CheckOtpForm from "../components/templates/CheckOtpForm";
 
 function AuthPage() {
-  const [step, setStep] = useState(2);
+  const [step, setStep] = useState(1);
   const [mobile, setMobile] = useState("");
   const [code, setCode] = useState("");
   return (
     <div>
       AuthPage
-      {step === 1 && <SendOtpForm />}
+      {step === 1 && (
+        <SendOtpForm setStep={setStep} mobile={mobile} setMobile={setMobile} />
+      )}
       {step === 2 && <CheckOtpForm />}
     </div>
   );
