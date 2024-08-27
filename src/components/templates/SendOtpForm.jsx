649ef9d26e77f764f63a1e67e7a468a24a92450b
shakila mohammadi <57131482+ShakilaMhd@users.noreply.github.com>
2024-08-27 21:07:13 +0330

    send the mobile number to the backend

--- src/components/templates/SendOtpForm.jsx
+++ src/components/templates/SendOtpForm.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { sendOtp } from "../../services/auth";
+
 
 function SendOtpForm({ mobile, setMobile, setStep }) {
-    const submitHandler = (event) => {
-        event.preventDefault()
-        console.log(event);
-    }
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    // console.log(mobile);
+
+    if (mobile.length !== 11) return;
+
+    const { response, error } = await sendOtp(mobile);
+    console.log({ response, error });
+    
+  };
   return (
     <form onSubmit={submitHandler}>
       <p>ورود به حساب کاربری</p>
@@ -13,8 +21,14 @@ function SendOtpForm({ mobile, setMobile, setStep }) {
         کدتایید شده به این شماره پیامک خواهد شد
       </span>
       <label htmlFor="input">ماره موبایل خود را وارد کنید</label>
-          <input type="text" id="input" placeholder="شماره موبایل" value={mobile} onChange={e => setMobile(e.target.value)} />
-          <button type="submit">ارسال کد تایید</button>
+      <input
+        type="text"
+        id="input"
+        placeholder="شماره موبایل"
+        value={mobile}
+        onChange={(e) => setMobile(e.target.value)}
+      />
+      <button type="submit">ارسال کد تایید</button>
     </form>
   );
 }
