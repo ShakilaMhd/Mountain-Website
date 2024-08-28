359b676a8fe5810a42e4ddb7a4a011072cf6867e
shakila mohammadi <57131482+ShakilaMhd@users.noreply.github.com>
2024-08-28 13:40:30 +0330

    create the config file for my absolute imports

---
+++ src/constants/paths.js
@@ -0,0 +1,12 @@
+export const paths = [
+        "src",
+        "assets",
+        "components",
+        "configs",
+        "layout",
+        "pages",
+        "router",
+        "services",
+        "styles",
+        "utils"
+    ]
