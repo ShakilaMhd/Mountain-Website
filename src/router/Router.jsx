203c7fd991054f68154704f7fc278b5270da8404
shakila mohammadi <57131482+ShakilaMhd@users.noreply.github.com>
2024-08-28 17:32:40 +0330

    access the access token and specify the role user or admin

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+
 import HomePage from "pages/HomePage";
 import DashboardPage from "pages/DashboardPage";
 import AdminPage from "pages/AdminPage";
 import AuthPage from "pages/AuthPage";
 import PageNotFound from "pages/404";
+import { getProfile } from "../services/user";
 
 function Router() {
+    const {data, isLoading, error} = useQuery({queryKey: ["profile"], queryFn:getProfile})
+    console.log({data, isLoading, error});
   return (
     <Routes>
       <Route index element={<HomePage />} />
