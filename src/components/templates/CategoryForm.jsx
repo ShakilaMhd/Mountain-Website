fe288307d78b7110620afda3845b95371c21249a
shakila mohammadi <57131482+ShakilaMhd@users.noreply.github.com>
2024-08-30 17:08:37 +0330

    invalidate the latest query

--- src/components/templates/CategoryForm.jsx
+++ src/components/templates/CategoryForm.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { addCategory } from "../../services/admin";
 
 import styles from "./CategoryForm.module.css";
 
 function CategoryForm() {
+  const queryClient = useQueryClient();
   const [form, setForm] = useState({ name: "", slug: "", icon: "" });
-  const { mutate, isLoading, error, data } = useMutation(addCategory);
+
+  const { mutate, isLoading, error, data } = useMutation(addCategory, {
+    onSuccess: () => queryClient.invalidateQueries("get-categories"),
+  });
   console.log({ isLoading, error, data });
 
   const changeHandler = (event) => {
