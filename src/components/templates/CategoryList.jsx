f6479479c15f34a1470d36870838b01835bd2631
shakila mohammadi <57131482+ShakilaMhd@users.noreply.github.com>
2024-08-30 15:51:51 +0330

    get all the categories

---
+++ src/components/templates/CategoryList.jsx
@@ -0,0 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import { getCategory } from "../../services/admin";
+import Loader from "../../module/Loader";
+
+function CategoryList() {
+  const { data, isLoading } = useQuery(["get-categories"], getCategory);
+  console.log({ data, isLoading });
+
+  return (
+    <div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        data.data.map((i) => (
+          <div key={i._id}>
+            <img src={`${i.icon}.svg`} alt="" />
+            <h5>{i.name}</h5>
+            <p>slug:{i.slug}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
+}
+
+export default CategoryList;
