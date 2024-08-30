525801fed362d744efa97db355f92c16b3c728d8
shakila mohammadi <57131482+ShakilaMhd@users.noreply.github.com>
2024-08-30 17:01:54 +0330

    add css to the category file

--- src/components/templates/CategoryList.jsx
+++ src/components/templates/CategoryList.jsx
@@ -2,13 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { getCategory } from "../../services/admin";
 import Loader from "../../module/Loader";
+import styles from "./CategoryList.module.css"
+
 
 function CategoryList() {
   const { data, isLoading } = useQuery(["get-categories"], getCategory);
   console.log({ data, isLoading });
 
   return (
-    <div>
+    <div className={styles.list}>
       {isLoading ? (
         <Loader />
       ) : (
