e149fc8c0e92f5d785bc013982b7788dd99e0f1a
shakila mohammadi <57131482+ShakilaMhd@users.noreply.github.com>
2024-08-30 21:07:53 +0330

    complete the homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import SideBar from "../components/templates/SideBar";
+import Main from "../components/templates/Main";
+import { useQuery } from "@tanstack/react-query";
+import { getAllPosts } from "../services/user";
+import Loader from "../module/Loader";
+import { getCategory } from "../services/admin";
+const style = { display: "flex" };
 
 function HomePage() {
+  const { data: posts, isLoading: postLoading } = useQuery(["post-list"], getAllPosts);
+  const { data: categories, isLoading: categoryLoading } = useQuery(["get-categories"], getCategory);
+  console.log(categories);
+  // console.log({ isLoading, data });
   return (
-    <div>HomePage</div>
-  )
+    <>
+      {postLoading || categoryLoading ? (
+        <Loader />
+      ) : (
+        <div style={style}>
+          <SideBar categories={categories} />
+          <Main posts={posts} />
+        </div>
+      )}
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
