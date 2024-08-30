93d5b6c24fb6eca03daf0453bb6190e5ca3bff1b
shakila mohammadi <57131482+ShakilaMhd@users.noreply.github.com>
2024-08-30 17:25:47 +0330

    create the form for the advertisement

---
+++ src/components/templates/AddPost.jsx
@@ -0,0 +1,39 @@
+import { useQuery } from "@tanstack/react-query";
+import { getCategory } from "../../services/admin";
+
+function AddPost() {
+  const { data } = useQuery(["get-categories"], getCategory);
+    console.log(data);
+    
+    const addHandler = (event) => {
+        event.preventDefault();
+        console.log("sent");
+    }
+  return (
+    <form action="">
+      <h3>افزودن آگهی</h3>
+      <label htmlFor="title">عنوان</label>
+      <input type="text" name="title" id="title" />
+      <label htmlFor="content">توضیحات</label>
+      <input type="text" name="content" id="content" />
+      <label htmlFor="amount">قیمت</label>
+      <input type="text" name="amount" id="amount" />
+      <label htmlFor="city">شهر</label>
+      <input type="text" name="city" id="city" />
+      <label htmlFor="category">دسته بندی</label>
+      <select name="category" id="category">
+        {data?.data.map((i) => (
+          <option key={i._id} value={i._id}>
+            {i.name}
+          </option>
+        ))}
+      </select>
+      <label htmlFor="images">عکس</label>
+          <input type="file" name="images" id="images" />
+          
+          <button onClick={addHandler}>ایجاد</button>
+    </form>
+  );
+}
+
+export default AddPost;
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import AddPost from '../components/templates/AddPost'
 
 function DashboardPage() {
   return (
-    <div>DashboardPage</div>
+    <div><AddPost /></div>
   )
 }
 
